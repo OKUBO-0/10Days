8a171d40123d687e107d90dc79f7bccdfb9b792e
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-09-11 15:36:00 +0900

    当たり判定改まで

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -86,7 +86,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"10days");
+	win->CreateGameWindow(L"反転世界");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
